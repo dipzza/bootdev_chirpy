17f074d6e61eb32151706e12dd171c896bddcc05
dipzza <dipzza@disroot.org>
2025-03-20 13:50:45 +0100

    Init basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dipzza/bootdev_chirpy
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write(fmt.Appendf(nil, "Hits: %v", cfg.fileserverHits.Load()))
+	})
+}
+
+func (cfg *apiConfig) reset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+	})
+}
+
+func main() {
+	apiCfg := apiConfig{}
+	serverMux := http.NewServeMux()
+	serverMux.Handle(
+		"/app/", 
+		http.StripPrefix("/app", 
+			apiCfg.middlewareMetricsInc(
+				http.FileServer(http.Dir(".")),
+			),
+		),
+	)
+
+	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	serverMux.Handle("/metrics", apiCfg.metrics())
+	serverMux.Handle("/reset", apiCfg.reset())
+	
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serverMux,
+	}
+	server.ListenAndServe()
+}
